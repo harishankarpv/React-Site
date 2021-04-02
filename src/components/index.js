068b4a4b4be8fb0ad8dd48311c8b96f3aaa0b5af
harishankarpv <harishankarpv99@gmail.com>
2021-04-02 17:29:46 +0530

    index.js
    modified index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,6 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as Footer } from './Footer/Footer';
 export { default as InfoSection } from './InfoSection/InfoSection';
 export { default as Pricing } from './Pricing/Pricing';
+
+
+// THis is a random comment
